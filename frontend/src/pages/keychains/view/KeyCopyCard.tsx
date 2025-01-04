f50423711de9b5c263ce5e5de395e89054f43d65
Ferdinand Antonius <ferdinandant@gmail.com>
2025-01-05 05:45:00 +0700

    View key copies

--- frontend/src/pages/keychains/view/KeyCopyCard.tsx
+++ frontend/src/pages/keychains/view/KeyCopyCard.tsx
@@ -4,12 +4,22 @@ import { Card, VStack, Box, Button, Text, HStack } from "@chakra-ui/react";
 
 type Props = {
   keyCopyData: any;
+  onClickUpdate?: (keyID: string) => any;
+  onClickDelete?: (keyID: string) => any;
 };
 
 export default function KeyCopyCard(props: Props) {
-  const { keyCopyData } = props;
+  const { keyCopyData, onClickUpdate, onClickDelete } = props;
   const { KeyID, DateCreated, StaffID, StaffName } = keyCopyData;
   const hasAssignedStaff = StaffID.Valid;
+
+  const handleClickUpdate = () => {
+    onClickUpdate && onClickUpdate(KeyID);
+  };
+
+  const handleClickDelete = () => {
+    onClickDelete && onClickDelete(KeyID);
+  };
 
   return (
     <Card.Root width={"full"}>
@@ -18,14 +28,16 @@ export default function KeyCopyCard(props: Props) {
           <div>
             <BsFillKeyFill size={48} />
           </div>
+
           <VStack gap={1} alignItems="stretch" flexGrow={1}>
+            {/* Title */}
             <div>
               <Text textStyle="lg" style={{ width: "100%", fontWeight: 600 }}>
                 {KeyID}
               </Text>
               <div style={{ color: "#334155" }}>Created at {DateCreated}</div>
             </div>
-
+            {/* Details */}
             <div
               style={{
                 color: "#64748b",
@@ -51,6 +63,20 @@ export default function KeyCopyCard(props: Props) {
                 </i>
               )}
             </div>
+            {/* Buttons */}
+            <HStack pt={2} justifyContent="flex-end">
+              <Button size="xs" variant="subtle" onClick={handleClickUpdate}>
+                Assign/unassign bearer
+              </Button>
+              <Button
+                size="xs"
+                variant="subtle"
+                colorPalette="red"
+                onClick={handleClickDelete}
+              >
+                Delete
+              </Button>
+            </HStack>
           </VStack>
         </HStack>
       </Box>
